f14fc3ce5c3a93bc5e63424473c96926464cb6c4
Prince Kalariya <117919642+Princek28@users.noreply.github.com>
2023-09-11 11:08:19 +0530

    My Portfolio
    Welcome to my portfolio, where creativity meets expertise! I am a Prince Kalariya with a passion for web design.
    
    Technologies :
    HTML,
    HTML5,
    CSS,
    CSS3,
    BOOTSTRAP

---
+++ main.js
@@ -0,0 +1,15 @@
+// var typed= new typed(".text", {
+//     Strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+
+// var typed= new typed(".text",{
+//     Strings:["Prince","Kalariya","Manojbhai"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// })
